10e57998850b0671fde36a97add1a08fa659a915
Nour-eddine <fattoumne@protonmail.com>
2022-11-23 12:03:06 +0100

    first java project initialisation

---
+++ first_project/src/first_java_project/app.java
@@ -0,0 +1,13 @@
+package first_java_project;
+
+public class app {
+	
+	public static void main(String[] args) {
+		System.out.println("bonjour");
+		System.out.println(addition(5, 10));
+	}
+
+	public static int addition (int a, int b) {
+		return a + b;
+	}
+}	
